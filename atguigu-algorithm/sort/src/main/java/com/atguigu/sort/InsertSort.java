fc96ab629b953f4eaf38e005705631c42458b9ac
miaoqi <miaoqi3@xdf.cn>
2023-04-16 18:43:58 +0800

    update blog

---
+++ atguigu-algorithm/sort/src/main/java/com/atguigu/sort/InsertSort.java
@@ -0,0 +1,63 @@
+package com.atguigu.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 插入排序
+ *
+ * 插入排序（Insertion Sorting）的基本思想是：把n个待排序的元素看成为一个有序表和一个无序表，
+ * 开始时有序表中只包含一个元素，无序表中包含有n-1个元素，
+ * 排序过程中每次从无序表中取出第一个元素，把它的排序码依次与有序表元素的排序码进行比较，
+ * 将它插入到有序表中的适当位置，使之成为新的有序表
+ *
+ * 即将一个数组分作左右两个部分, 从右边无序的第一个向左边有序的逐次查找插入
+ *
+ * @author miaoqi
+ * @date 2023-04-15 21:16:7
+ */
+public class InsertSort {
+
+    public static void main(String[] args) {
+        // int[] arr = {101, 34, 119, 1, -1, 89};
+        // 创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+        }
+
+        System.out.println("插入排序前");
+        // System.out.println(Arrays.toString(arr));
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        insertSort(arr); //调用插入排序算法
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        System.out.println("插入排序后");
+        // System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertSort(int[] arr) {
+        // 待插入的值
+        int insertVal = 0;
+        // 从右边第一个位置开始遍历
+        for (int i = 1; i < arr.length; i++) {
+            insertVal = arr[i];
+            // 从左边最后一个位置向前查找, 直到找到一个比当前小的数就交换
+            // 如果下标合法, 并且最小值小于左边的值, 就将左边的值右移
+            int j;
+            for (j = i - 1; j >= 0 && arr[j] < insertVal; j--) {
+                arr[j + 1] = arr[j];
+            }
+            // 因为 j-- 比较之后不合法, 所以要 +1 赋值
+            arr[j + 1] = insertVal;
+        }
+    }
+
+}
---
+++ atguigu-algorithm/sort/src/main/java/com/atguigu/sort/ShellSort.java
@@ -0,0 +1,152 @@
+package com.atguigu.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 希尔排序
+ *
+ * 我们看简单的插入排序可能存在的问题.
+ * 数组 arr = {2,3,4,5,6,1} 这时需要插入的数 1(最小), 这样的过程是：
+ * {2,3,4,5,6,6}
+ * {2,3,4,5,5,6}
+ * {2,3,4,4,5,6}
+ * {2,3,3,4,5,6}
+ * {2,2,3,4,5,6}
+ * {1,2,3,4,5,6}
+ * 结论: 当需要插入的数是较小的数时，后移的次数明显增多，对效率有影响
+ *
+ * 希尔排序是希尔（Donald Shell）于1959年提出的一种排序算法。希尔排序也是一种插入排序，它是简单插入排序经过改进之后的一个更高效的版本，也称为缩小增量排序。
+ *
+ * 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止
+ *
+ * @author miaoqi
+ * @date 2023-04-16 1:30:57
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+        // int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+
+        // int[] arr = { 8, 9, 1, 7, 2, 3, 5, 4, 6, 0 };
+
+        // 创建要给80000个的随机的数组
+        int[] arr = new int[8000000];
+        for (int i = 0; i < 8000000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+        }
+
+        // System.out.println("排序前");
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        // shellSort(arr); // 交换式
+        shellSort2(arr); // 移位方式
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        // System.out.println(Arrays.toString(arr));
+    }
+
+    // 交换法
+    public static void shellSort(int[] arr) {
+        int temp = 0;
+        int count = 0;
+        // 根据前面的逐步分析，使用循环处理, gap 即步长, 每次 gap / 2, 直到 gap = 1 为止
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从 gap 位置开始向后遍历
+            for (int i = gap; i < arr.length; i++) {
+                // 遍历各组中所有的元素(共 gap 组，每组有个元素), 步长 gap, 所以每次
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    // 如果当前元素大于加上步长后的那个元素，说明交换
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+            //System.out.println("希尔排序第" + (++count) + "轮 =" + Arrays.toString(arr));
+        }
+
+		/*
+
+		// 希尔排序的第1轮排序
+		// 因为第1轮排序，是将10个数据分成了 5组
+		for (int i = 5; i < arr.length; i++) {
+			// 遍历各组中所有的元素(共5组，每组有2个元素), 步长5
+			for (int j = i - 5; j >= 0; j -= 5) {
+				// 如果当前元素大于加上步长后的那个元素，说明交换
+				if (arr[j] > arr[j + 5]) {
+					temp = arr[j];
+					arr[j] = arr[j + 5];
+					arr[j + 5] = temp;
+				}
+			}
+		}
+
+		System.out.println("希尔排序1轮后=" + Arrays.toString(arr));//
+
+
+		// 希尔排序的第2轮排序
+		// 因为第2轮排序，是将10个数据分成了 5/2 = 2组
+		for (int i = 2; i < arr.length; i++) {
+			// 遍历各组中所有的元素(共5组，每组有2个元素), 步长5
+			for (int j = i - 2; j >= 0; j -= 2) {
+				// 如果当前元素大于加上步长后的那个元素，说明交换
+				if (arr[j] > arr[j + 2]) {
+					temp = arr[j];
+					arr[j] = arr[j + 2];
+					arr[j + 2] = temp;
+				}
+			}
+		}
+
+		System.out.println("希尔排序2轮后=" + Arrays.toString(arr));//
+
+		// 希尔排序的第3轮排序
+		// 因为第3轮排序，是将10个数据分成了 2/2 = 1组
+		for (int i = 1; i < arr.length; i++) {
+			// 遍历各组中所有的元素(共5组，每组有2个元素), 步长5
+			for (int j = i - 1; j >= 0; j -= 1) {
+				// 如果当前元素大于加上步长后的那个元素，说明交换
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+
+		System.out.println("希尔排序3轮后=" + Arrays.toString(arr));//
+		*/
+    }
+
+
+    //对交换式的希尔排序进行优化 -> 移位法
+    public static void shellSort2(int[] arr) {
+        // 增量gap, 并逐步的缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        //移动
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    //当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+}

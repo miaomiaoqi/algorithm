4120997562bee02ffb7a6597d53b9318a8d0da22
miaoqi <miaoqi3@xdf.cn>
2024-01-28 01:03:09 +0800

    update blog

---
+++ imooc-liuyubobobo-algorithm/lru/src/main/java/com/miaoqi/algorithm/lru/LRUCacheByLinkedHashMap.java
@@ -0,0 +1,11 @@
+package com.miaoqi.algorithm.lru;
+
+import java.util.LinkedHashMap;
+
+public class LRUCacheByLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+
+    public static void main(String[] args) {
+
+    }
+
+}
